04ca641464fd49db963bc2b5d5179104eccb04c8
priyashamy444 <84502487+priyashamy444@users.noreply.github.com>
2021-05-25 21:27:26 +0530

    Add files via upload

---
+++ Sprin boot proj/project1app.java
@@ -0,0 +1,43 @@
+package com.example.project1;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.CommandLineRunner;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import com.example.project1.model.User;
+
+import com.example.project1.repository.UserRepository;
+
+
+@SpringBootApplication
+
+public class Project1Application implements CommandLineRunner {
+
+	
+public static void main(String[] args) {
+		
+SpringApplication.run(Project1Application.class, args);
+	}
+   
+ @Autowired
+	
+private UserRepository userRepository;
+
+	
+ @Override
+    public void run(String...args) throws Exception {
+        this.userRepository.save(new User("Priya", "shanmugam", "priya@gmail.com"));
+        this.userRepository.save(new User("shan", "priya", "shammy@gmail.com"));
+        this.userRepository.save(new User("arun", "arun", "arun@gmail.com"));
+        this.userRepository.save(new User("arasu", "P", "arasu@gmail.com"));
+        this.userRepository.save(new User("karthi", "M", "karthi@gmail.com"));
+        this.userRepository.save(new User("riya", "priya", "riya@gmail.com"));
+
+    }
+}
---
+++ Sprin boot proj/usercontroller.java
@@ -0,0 +1,23 @@
+package com.example.project1.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.project1.model.User;
+import com.example.project1.repository.UserRepository;
+
+@RestController
+@RequestMapping("api/")
+public class UserController {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("users")
+public List<User> getUsers(){
+		return this.userRepository.findAll();
+	
+}
+}
